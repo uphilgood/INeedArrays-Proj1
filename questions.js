35f89a188fc94b5966f8dc11c1c0bebb2b1016a9
ccalilung <brobeans289@gmail.com>
2018-07-06 23:59:42 -0400

    cleaned up questions, remove next button

--- questions.js
+++ questions.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
 
-var i = 0;
-var diet = '';
+    var i = 0;
+    var diet = '';
     var ingredients = null;
-    
+
     var restrictions = null;
     var caloric = null;
     var timeToCook = null;
@@ -70,7 +70,7 @@ var diet = '';
                         parameter: newQuestions[i].parameter
                     }).text(newQuestions[i].answers[x]).addClass("waves-effect waves-light light-blue btn-small buttonAnswers")
                     $("#buttons-view").append(button);
-                    
+
                     button = $("<button>")
 
                 }
@@ -81,7 +81,8 @@ var diet = '';
 
 
 
-        }i++;
+        }
+        i++;
     }
     defineVariables();
 
@@ -89,7 +90,7 @@ var diet = '';
 
         if ($(this).attr("parameter") == "diet") {
             diet = $(this).val();
-            dietURL = "&diet=" + diet 
+            dietURL = "&diet=" + diet
 
         }
         if ($(this).attr("parameter") == "ingredients") {
@@ -101,21 +102,26 @@ var diet = '';
         if ($(this).attr("parameter") == "timeToCook") {
             if ($(this).val() === '0-30') {
                 timeToCook = "30"
-            } if ($(this).val() === '30-60') {
+            }
+            if ($(this).val() === '30-60') {
                 timeToCook = "60"
-            } if ($(this).val() === 'All the Time in the World') {
+            }
+            if ($(this).val() === 'All the Time in the World') {
                 timeToCook = ""
 
-            } timeToCookURL = "&time=" + timeToCook;
-    }
+            }
+            timeToCookURL = "&time=" + timeToCook;
+        }
         if ($(this).attr("parameter") == "caloric") {
             if ($(this).val() === '0-1000') {
                 caloric = "1000"
-            } if ($(this).val() === '1000-1500') {
+            }
+            if ($(this).val() === '1000-1500') {
                 caloric = "1000-1500"
-            } if ($(this).val() === 'YOLO') {
+            }
+            if ($(this).val() === 'YOLO') {
                 caloric = "1501-6000"
-            } 
+            }
             caloricURL = "&calories=" + caloric
         }
         if ($(this).attr("parameter") == "restrictions") {
@@ -128,96 +134,109 @@ var diet = '';
 
             $("#question").html("");
             $("#buttons-view").html("");
+            $("#next-question").remove();
             quiz();
             $("#resultsButton").on("click", function () {
-                
+
             })
 
         }
-        
+
         defineVariables();
     }
 
     $(document).on("click", ".giffy", function () {
-        
-       
+
+
         if (this.getAttribute("id") === germany) {
             country = "german"
-        } if (this.getAttribute("id") === mexico) {
+        }
+        if (this.getAttribute("id") === mexico) {
             country = "mexican"
-        } if (this.getAttribute("id") === america) {
+        }
+        if (this.getAttribute("id") === america) {
             country = "american"
-        } if (this.getAttribute("id") === japan) {
+        }
+        if (this.getAttribute("id") === japan) {
             country = "japanese"
-        } if (this.getAttribute("id") === france) {
+        }
+        if (this.getAttribute("id") === france) {
             country = "french"
-        } 
+        }
 
         defineVariables();
 
     })
 
     $(document).on("click", ".buttonAnswers", checkButtonClick);
-    $("#next-question").on("click",function () {
-        defineVariables();
+    $("#next-question").on("click", function () {
+        if (i < 6) {
+            defineVariables();
+        }
+        if (i === 6) {
+            checkButtonClick();
+        }
     })
 
-function quiz() {
-    if (ingredients === null) {
-        ingredientURL = ""
-    } if (diet === null) {
-        dietURL = ""
-    } if (timeToCook === null) {
-        timeToCookURL = ""
-    } if (caloric === null) {
-        caloricURL = ""
-    } if (restrictions === null) {
-        restrictionsURL = ""
-    }
-    var newCountry = country
-    // var queryURL = "https://api.edamam.com/search?q=pork&app_id=cc37353f&app_key=36c506cb4523a2b0efb3a66e52109bdd&from=0&to=2"
-    var queryURL = "https://api.edamam.com/search?q=" + newCountry + "&app_id=cc37353f&app_key=36c506cb4523a2b0efb3a66e52109bdd&from=0&to=5" + ingredientURL + dietURL + timeToCookURL + caloricURL + restrictionsURL;
+    function quiz() {
+        if (ingredients === null) {
+            ingredientURL = ""
+        }
+        if (diet === null) {
+            dietURL = ""
+        }
+        if (timeToCook === null) {
+            timeToCookURL = ""
+        }
+        if (caloric === null) {
+            caloricURL = ""
+        }
+        if (restrictions === null) {
+            restrictionsURL = ""
+        }
+        var newCountry = country
+
+        var queryURL = "https://api.edamam.com/search?q=" + newCountry + "&app_id=cc37353f&app_key=36c506cb4523a2b0efb3a66e52109bdd&from=0&to=5" + ingredientURL + dietURL + timeToCookURL + caloricURL + restrictionsURL;
         $.ajax({
             url: queryURL,
             method: "GET",
             dataType: "jsonp",
         }).then(function (response) {
             var results = response.hits
-            randomNum = Math.floor(Math.random() * 5)
             var label = results[1].recipe.label
-            $("#question").html("<a href='"+results[1].recipe.url+"'>"+label+"</a>")
-            
-            for (j=0;j<results[1].recipe.ingredientLines.length;j++){
-            var ingredientLines = results[1].recipe.ingredientLines[j]
-            $("#buttons-view").append("<br>" + ingredientLines + "<br/>").css("color", "white")
+            $("#question").html("<a href='" + results[1].recipe.url + "'target = '_blank'>" + label + "</a>")
+
+            for (j = 0; j < results[1].recipe.ingredientLines.length; j++) {
+                var ingredientLines = results[1].recipe.ingredientLines[j]
+                $("#buttons-view").append("<br>" + ingredientLines + "<br/>").css("color", "white")
             }
-            
-                
-            }).fail(function (response){
-                $("#question").html("Oh No, we have no selections based on your input? <a href='http://uphilgood.github.io/INeedArrays-Proj1/#modal1'>Try Again?</a>")
-            })
-        
-}
 
 
-function gifs() {
-    var cuisines = ["Mexico", "Germany", "Japan", "USA", "France"]
-
-    for (m = 0; m < cuisines.length; m++) {
-        var cuisine = cuisines[m]
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=lDk3q639ddTqVr5NUUFSZEx1u8BpTfwc&q=" + cuisine + "&limit=1&offset=0&rating=G&lang=en";
-        $.ajax({
-            url: queryURL,
-            method: "GET",
-        }).then(response => {
-            var results = response.data[0]
-
-
-            var buttonPlace = $("#buttons-view")
-            buttonPlace.append("<img src='" + results.images.fixed_height.url + "' width='175' class='giffy' id='"+ results.id + "'value='" + cuisine+ "'/><br>")
-
+        }).fail(function (response) {
+            $("#question").html("Oh No, we have no selections based on your input? <a href='http://uphilgood.github.io/INeedArrays-Proj1/#modal1'>Try Again?</a>")
         })
 
     }
-}
+
+
+    function gifs() {
+        var cuisines = ["Mexico", "Germany", "Japan", "USA", "France"]
+
+        for (m = 0; m < cuisines.length; m++) {
+            var cuisine = cuisines[m]
+            var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=lDk3q639ddTqVr5NUUFSZEx1u8BpTfwc&q=" + cuisine + "&limit=1&offset=0&rating=G&lang=en";
+            $.ajax({
+                url: queryURL,
+                method: "GET",
+            }).then(response => {
+                var results = response.data[0]
+
+
+                var buttonPlace = $("#buttons-view")
+                buttonPlace.append("<img src='" + results.images.fixed_height.url + "' width='175' class='giffy' id='" + results.id + "'value='" + cuisine + "'/><br>")
+
+            })
+
+        }
+    }
 })
